79c968622e4033a7f5efab1115016549a595311c
michael <michael>
2026-01-03 06:31:03 -0700

    update attack.rs

--- adversary/src/attack.rs
+++ adversary/src/attack.rs
@@ -5,7 +5,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 pub static TRIGGER_NETWORK_INTERRUPT: AtomicBool = AtomicBool::new(false);
 pub const NETWORK_PARTITION: bool = true;
 
-pub const ATTACK_START_TIME_SEC: u64 = 50;
+pub const ATTACK_START_TIME_SEC: u64 = 40;
 pub const ATTACK_DURATION_SEC: u64 = 30;
 
 pub const GROUP: [usize; 10] = [0, 1, 1, 0, 1, 1, 0, 0, 0, 0];
